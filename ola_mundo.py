97167d7b373bff15b053d2b65ea1e1e41ae80708
Carina Carvalho Dias <carinadias.dev@gmail.com>
2026-03-16 22:57:47 -0300

    Create ola_mundo.py
    Um pequeno exercício de boas vindas.

---
+++ ola_mundo.py
@@ -0,0 +1,8 @@
+# Meu primeiro código em Python
+nome = "Carina Dias"
+print(f" Olá mundo! Aqui é a {nome} começando a jornada na programção.")
+
+# Um pequeno teste de lógica.
+estudando = ["Python", "MySQL", " Lógica"]
+for item in estudando:
+  print(f"Hoje estou praticando: {item}"
